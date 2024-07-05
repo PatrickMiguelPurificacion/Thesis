67a35ee9942593db72b96973c5000e102bafab8b
Emman Evangelista <emmanevangelista129@gmail.com>
2024-07-05 13:57:57 +0800

    simplify navbar using nav link builder

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -11,6 +11,26 @@ export default function NavBar() {
 
   const router = useRouter();
 
+  const navLinks : string[][] = [
+    ['Home', '/home'],
+    ['Flashcards', '/flashcard-decks'],
+    ['Kanban Board', '/kanban'],
+    ['Learn Page', '/learn'],
+    ['Notebook', '/notebook'],
+  ];
+
+  const getNavLink = (text: string, path: string) => (
+    <button
+      key={`nav-${path}`}
+      onClick={() => router.push(path)}
+      className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
+    >
+      {text}
+    </button>
+  );
+
+  // "text-white w-full hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
+
   return (
     <div className="flex-none flex-col h-screen text-white w-1/5 overflow-y-auto" style={{ backgroundColor: '#142059' }}>
 
@@ -19,60 +39,21 @@ export default function NavBar() {
       </h1>
       
       <div className="flex flex-col space-y-4">
-        <button 
-          onClick={() => router.push('/home')} 
-          className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
-        >
-          Home
-        </button>
-        
-        <button 
-          onClick={() => router.push('/flashcard-decks')} 
-          className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
-        >
-          Flashcards
-        </button>
-        <button 
-          onClick={() => router.push('/kanban')} 
-          className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
-        >
-          Kanban Board
-        </button>
-        <button 
-          onClick={() => router.push('/learn')} 
-          className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
-        >
-          Learn Page
-        </button>
-        <button 
-          onClick={() => router.push('/notebook')} 
-          className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
-        >
-          Notebook
-        </button>
+        {navLinks.map((val: string[]) => getNavLink(val[0], val[1]))}
       </div>
 
       {session?.data?.snapshot?.admin && 
-        <div className="pt-4 border-t border-gray-700 mt-4 space-y-4">
-          <button
-            onClick={() => router.push('/admin/decks')} 
-            className="text-white w-full hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
-          >
-            Global Flashcards
-          </button>
+        <div className="flex flex-col pt-4 border-t border-gray-700 mt-4 space-y-4">
+          {getNavLink('Global Flashcards', '/admin/decks')}
         </div>
       }
       
-      <div className="py-4 border-t border-gray-700 mt-4 space-y-4">
-        <button 
-          onClick={() => router.push('/user-profile')} 
-          className="text-white w-full hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
-        >
-          User Profile
-        </button>
-        <button 
+      <div className="flex flex-col py-4 border-t border-gray-700 mt-4 space-y-4">
+        {getNavLink('User Profile', '/user-profile')}
+
+        <button
           onClick={() => signOut()}
-          className="text-white w-full hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
+          className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
         >
           Sign Out
         </button>
