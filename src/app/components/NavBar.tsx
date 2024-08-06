7adbe22ae174b6128bf77e23fd5b7af55dc6640d
Emman Evangelista <emmanevangelista129@gmail.com>
2024-08-07 05:39:31 +0800

    modifications for admin/user

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -15,14 +15,6 @@ export default function NavBar() {
 
   const router = useRouter();
 
-  const navLinks : string[][] = [
-    ['Home', '/home'],
-    ['Flashcards', '/flashcard-decks'],
-    ['Kanban Board', '/kanban'],
-    ['Learn Page', '/learn'],
-    ['Notebook', '/notebook'],
-  ];
-
   const getNavLink = (text: string, path: string) => (
     <button
       key={`nav-${path}`}
@@ -32,8 +24,6 @@ export default function NavBar() {
       {text}
     </button>
   );
-
-  // "text-white w-full hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
 
   return (
     <nav className={`${navOpen ? 'w-fit' : 'w-[80px]'} sm:w-1/5 flex-none flex-col h-screen text-white overflow-y-auto transition duration-300 ease-in-out`} style={{ backgroundColor: '#142059' }}>
@@ -48,28 +38,42 @@ export default function NavBar() {
           Navigation
         </h1>
       </div>
-      
-      <div className={`${navOpen ? 'flex' : 'hidden'} sm:flex flex-col gap-y-4 items-stretch py-4`}>
-        {navLinks.map((val: string[]) => getNavLink(val[0], val[1]))}
-      </div>
 
-      {session?.data?.snapshot?.admin && 
-        <div className={`${navOpen ? 'flex' : 'hidden'} sm:flex flex-col gap-y-4 items-stretch py-4 border-t border-gray-700`}>
-          {getNavLink('Global Flashcards', '/admin/decks')}
-          {getNavLink('All Users', '/admin/users')}
-        </div>
-      }
-      
-      <div className={`${navOpen ? 'flex' : 'hidden'} sm:flex flex-col gap-y-4 items-stretch py-4 border-t border-gray-700`}>
-        {getNavLink('User Profile', '/user-profile')}
+      {session.status === "authenticated" && (
+        <>
+          {session?.data?.snapshot?.admin
+            ? (
+              <div className={`${navOpen ? 'flex' : 'hidden'} sm:flex flex-col gap-y-4 items-stretch py-4 border-t border-gray-700`}>
+                {getNavLink('Home', '/home')}
+                {getNavLink('Global Flashcards', '/admin/decks')}
+                {getNavLink('All Users', '/admin/users')}
+              </div>
+            ) : (
+              <>
+                <div className={`${navOpen ? 'flex' : 'hidden'} sm:flex flex-col gap-y-4 items-stretch py-4`}>
+                  {getNavLink('Home', '/home')}
+                  {getNavLink('Flashcards', '/flashcard-decks')}
+                  {getNavLink('Kanban Board', '/kanban')}
+                  {getNavLink('Learn Page', '/learn')}
+                  {getNavLink('Notebook', '/notebook')}
+                </div>
+              </>
+            )
+          }
 
-        <button
-          onClick={() => signOut()}
-          className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
-        >
-          Sign Out
-        </button>
-      </div>
+          <div className={`${navOpen ? 'flex' : 'hidden'} sm:flex flex-col gap-y-4 items-stretch py-4 border-t border-gray-700`}>
+            {!session?.data?.snapshot?.admin && getNavLink('User Profile', '/user-profile')}
+
+            <button
+              onClick={() => signOut()}
+              className="nav-link hover:bg-blue-500 py-2 px-6 transition duration-300 ease-in-out"
+            >
+              Sign Out
+            </button>
+          </div>
+        </>
+      )}
+      
     </nav>
   );
 }
